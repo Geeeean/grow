f0e91ffab048f2a94339c15da9880bde06c470ac
Gianluca Violoni <violoni.gianluca@gmail.com>
2024-10-13 02:55:18 +0200

    +: tailwind, tanstack router, shadcn

--- client/src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-    return <>GROW</>;
-}
-
-export default App;
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +9,10 @@ export default defineConfig({
         outDir: '../dist/client',
         emptyOutDir: true,
     },
-    plugins: [react()],
+    plugins: [react(), TanStackRouterVite()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
 });
