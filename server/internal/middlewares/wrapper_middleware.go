6a62bacab0176d38e66e9eba70b31cdb2ec96614
Geeeean <violoni.gianluca@gmail.com>
2024-11-16 05:49:36 +0100

    middleware wrapper fix

--- server/internal/middlewares/wrapper_middleware.go
+++ server/internal/middlewares/wrapper_middleware.go
@@ -1,37 +1,38 @@
 package middlewares
 
 import (
-	"github.com/Geeeean/grow/internal/api"
 	"net/http"
+
+	"github.com/Geeeean/grow/internal/api"
 )
 
-func isOriginAllowed(_ string) bool {
+func isOriginAllowed(origin string) bool {
 	return true
 }
 
 func Wrapper(method string, next api.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != method {
-			if err := api.NewError(http.StatusMethodNotAllowed, "Method Not Allowed").JSON(w); err != nil {
-				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-			}
-			return
-		}
-
 		origin := r.Header.Get("Origin")
 
 		if isOriginAllowed(origin) {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 		}
 
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
 			return
 		}
 
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		if r.Method != method {
+			if err := api.NewError(http.StatusMethodNotAllowed, "Method Not Allowed").JSON(w); err != nil {
+				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			}
+			return
+		}
 
 		if err := next(w, r).JSON(w); err != nil {
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
