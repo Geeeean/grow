249a34405fe2b3c082fbc370a43a9e8bd2f386b0
Geeeean <violoni.gianluca@gmail.com>
2024-12-11 01:59:02 +0100

    api endpoint fixing (wip)

--- client/src/services/api/user.ts
+++ client/src/services/api/user.ts
@@ -1,6 +1,6 @@
 import { apiClient } from '@/services/api/client';
 
 export const info = async () => {
-    const response = await apiClient.get('/user/info');
+    const response = await apiClient.get('/user');
     return response.data;
 };
